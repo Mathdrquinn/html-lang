fa90d5bbc99d8e8df8119e60a9cbf44047671ff8
Brendan Quinn <Brendan.Quinn@brooks.com>
2020-03-27 11:53:01 -0400

    react-suer with throttle

--- src/container/DisplayLang.component.jsx
+++ src/container/DisplayLang.component.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useMemo } from 'react';
-import {  Combobox,  ComboboxInput,  ComboboxPopover,  ComboboxList,  ComboboxOption, ComboboxOptionText } from "@reach/combobox";import "@reach/combobox/styles.css";
+import {  Combobox,  ComboboxInput,  ComboboxPopover,  ComboboxList,  ComboboxOption } from "@reach/combobox";import "@reach/combobox/styles.css";
 import { LangContext, SUCCESS } from "../context/lang.context"
 import { useContext } from "react"
+import { useThrottle } from 'react-use';
 
 export const DisplayLang = () => {
-    const [term, setTerm] = useState('');
     const lang = useContext(LangContext);
+    const [term, setTerm] = useState('');
+    const throttledTerm = useThrottle(term, 150);
     const matchingTags = useMemo(() => {
         if(lang.status !== SUCCESS)
             return [];
         return Object.keys(lang.data.language)
         .map(k => lang.data.language[k])
-        .filter(tag => tag.Description.toLowerCase().includes(term.toLowerCase()) || tag.Subtag.toLowerCase().includes(term.toLowerCase()))
+        .filter(tag => tag.Description.toLowerCase().includes(throttledTerm.toLowerCase()) || tag.Subtag.toLowerCase().includes(throttledTerm.toLowerCase()))
         .slice(0, 8)
-    }, [term, lang])
+    }, [throttledTerm, lang])
 
     if (lang.status !== SUCCESS)
         return `⌚`;
